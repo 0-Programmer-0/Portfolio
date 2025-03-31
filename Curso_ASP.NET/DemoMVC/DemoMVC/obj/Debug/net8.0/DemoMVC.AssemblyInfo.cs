f2496b668ac41529f4788f2fafc01c1bbf137bf0
unknown <jmoraes2003@hotmail.com>
2025-03-31 13:55:23 -0300

    novos projetos ASP.NET

--- Curso_ASP.NET/DemoMVC/DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ Curso_ASP.NET/DemoMVC/DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef7e9e163a2ca1065eacb9a263855fe82dc96b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
